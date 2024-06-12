cfb077dc8a16f5759816845d0dd27a86a6251f0f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-12 16:13:49 +0500

    feat: add HomePage page

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:skar_admin/helpers/methods/navigation.dart';
+import 'package:skar_admin/pages/home.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -12,15 +14,15 @@ class _StartPageState extends State<StartPage> {
   @override
   void initState() {
     super.initState();
-    // _moveNextPage();
+    _moveNextPage();
   }
 
-  // _moveNextPage() async {
-  //   await Future.delayed(const Duration(seconds: 1));
-  //   if (mounted) {
-  //     goToPage(context, const HomePage(), true);
-  //   }
-  // }
+  _moveNextPage() async {
+    await Future.delayed(const Duration(seconds: 1));
+    if (mounted) {
+      goToPage(context, const HomePage(), true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
