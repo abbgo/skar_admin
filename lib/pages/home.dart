4ea9c7e4eee14e887e5757bed6f764faa2671ad4
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-12 16:33:43 +0500

    feat: add LoginPage page and update home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/pages/login/login.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    String token = ref.read(tokenProvider);
+
+    return Scaffold(
+      body: token.isEmpty
+          ? const LoginPage()
+          : const Center(child: Text('Home Page')),
     );
   }
 }
