400512028adcec60b36016b7b301401813bd6c42
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-12 16:00:23 +0500

    feat: add StartPage page

---
+++ lib/pages/start.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  // INIT STATE ----------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+    // _moveNextPage();
+  }
+
+  // _moveNextPage() async {
+  //   await Future.delayed(const Duration(seconds: 1));
+  //   if (mounted) {
+  //     goToPage(context, const HomePage(), true);
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo.jpg"),
+            Image.asset("assets/animated_icons/loading.gif", fit: BoxFit.cover),
+          ],
+        ),
+      ),
+    );
+  }
+}
