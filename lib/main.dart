1276d11e2edecbd78e3df7188c817ba2556213c7
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-12 16:23:35 +0500

    feat: update main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/pages/start.dart';
 import 'package:skar_admin/styles/theme/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    // APP ONLY DISPLAY PORTRAIT
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await dotenv.load(fileName: ".env"); // LOAD .ENV FILE
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
