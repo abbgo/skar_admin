9f685491e59930221d4e8d00a60b0c50cc5c4dfd
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-21 21:37:40 +0500

    feat: add loginShopOwner function

--- lib/services/api/shop_owner.dart
+++ lib/services/api/shop_owner.dart
@@ -1,9 +1,25 @@
+import 'dart:convert';
+
 import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop_owner.dart';
+import 'package:http/http.dart' as http;
 
 class ShopOwnerApiService {
-  Future<ShopOwner> login() async {
+  Future<ShopOwner> loginShopOwner(String phoneNumber, String password) async {
     Uri uri = Uri.parse('$apiUrl/shop-owners/login');
+
+    try {
+      http.Response response = await http.post(
+        uri,
+        body: {'phone_number': phoneNumber, 'password': password},
+      );
+      dynamic jsonData = json.decode(response.body);
+      print('------------------');
+      print(jsonData);
+    } catch (e) {
+      print('-------------------');
+      print(e.toString());
+    }
 
     return ShopOwner.defaultShopOwner();
   }
