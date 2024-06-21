f71dd846d2b1bbd4a9f06fe9ec2f8d1bff2358eb
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-21 21:30:57 +0500

    feat: add apiUrl and pathUrl const variables

--- lib/services/api/shop_owner.dart
+++ lib/services/api/shop_owner.dart
@@ -1,7 +1,10 @@
+import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop_owner.dart';
 
 class ShopOwnerApiService {
   Future<ShopOwner> login() async {
+    Uri uri = Uri.parse('$apiUrl/shop-owners/login');
+
     return ShopOwner.defaultShopOwner();
   }
 }
