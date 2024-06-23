1ca1cc4a805e6ac3582e1ea4c1466e84ad3f4afb
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-23 12:07:58 +0500

    feat: add ShopApiService service class and add fetchShops function

--- lib/services/api/shop.dart
+++ lib/services/api/shop.dart
@@ -1,41 +1,43 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 import 'package:http/http.dart' as http;
+import 'package:skar_admin/helpers/static_data.dart';
+import 'package:skar_admin/models/shop.dart';
 
-// class ShopApiService {
-//   // fetch shops -------------------------------------------------------
-//   Future<List<Shop>> fetchShops({
-//     required int page,
-//     required bool isRandom,
-//     required String search,
-//     required String lang,
-//   }) async {
-//     Uri uri = Uri.parse('$apiUrl/shops').replace(
-//       queryParameters: {
-//         'limit': '10',
-//         'page': '$page',
-//         'is_random': isRandom.toString(),
-//         'search': search,
-//         'lang': lang,
-//       },
-//     );
+class ShopApiService {
+  // fetch shops -------------------------------------------------------
+  Future<List<Shop>> fetchShops({
+    required int page,
+    required String shopOwnerID,
+    required bool isDeleted,
+  }) async {
+    Uri uri = Uri.parse('$apiUrl/back/shops').replace(
+      queryParameters: {
+        'limit': '10',
+        'page': '$page',
+        'shop_owner_id': shopOwnerID,
+        'is_deleted': '$isDeleted',
+      },
+    );
 
-//     try {
-//       Response response = await http.get(uri);
-//       var jsonData = json.decode(response.body);
+    try {
+      http.Response response = await http.get(uri);
+      var jsonData = json.decode(response.body);
 
-//       if (response.statusCode == 200 && jsonData['status']) {
-//         if (jsonData['shops'] == null) return [];
-//         var shopsList = jsonData['shops'] as List;
-//         return shopsList
-//             .map<Shop>((propJson) => Shop.fromJson(propJson))
-//             .toList();
-//       }
-//       return [];
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-// }
+      if (response.statusCode == 200 && jsonData['status']) {
+        if (jsonData['shops'] == null) return [];
+        var shopsList = jsonData['shops'] as List;
+        return shopsList
+            .map<Shop>((propJson) => Shop.fromJson(propJson))
+            .toList();
+      }
+      return [];
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
 
 class ShopParams extends Equatable {
   final String? shopOwnerID;
