3ed038a219fc573dfb79cbe861febddf19123dd6
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-23 20:46:09 +0500

    fix: update AddShopButton part widget

--- lib/pages/parts/add_shop_button.dart
+++ lib/pages/parts/add_shop_button.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AddShopButton extends StatelessWidget {
   const AddShopButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return ElevatedButton(
       style: ElevatedButton.styleFrom(elevation: 5),
       onPressed: () {},
-      child: const Text('Taze Dukan gos'),
+      child: Text(lang.addNewShop),
     );
   }
 }
