d143e2cd9be66671689ff830278aba3ebd3f9874
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-23 20:53:30 +0500

    fix: update AddShopPage page

--- lib/pages/add_shop/add_shop.dart
+++ lib/pages/add_shop/add_shop.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AddShopPage extends StatelessWidget {
   const AddShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    var lang = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(lang.addShopInformation),
+      ),
+      body: const Center(
         child: Text('Add new shop page'),
       ),
     );
