8c3b554ea3cc93eb30b845463d69a665642f95a0
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-24 16:58:15 +0500

    feat: add InputPart part widget

--- lib/pages/parts/input_part.dart
+++ lib/pages/parts/input_part.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/helpers/methods/parts/input.dart';
 import 'package:skar_admin/styles/colors.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ShopNameRuInput extends StatelessWidget {
-  const ShopNameRuInput({super.key, required this.nameRUCtrl});
+class InputPart extends StatelessWidget {
+  const InputPart({super.key, required this.ctrl, this.validatorFunc});
 
-  final TextEditingController nameRUCtrl;
+  final TextEditingController ctrl;
+  final String? Function(String?)? validatorFunc;
 
   @override
   Widget build(BuildContext context) {
-    var lang = AppLocalizations.of(context)!;
-
     return ConstrainedBox(
       constraints: const BoxConstraints(maxHeight: 80.0, minHeight: 80.0),
       child: TextFormField(
-        controller: nameRUCtrl,
+        controller: ctrl,
         keyboardType: TextInputType.text,
+        autofocus: true,
         textAlignVertical: TextAlignVertical.center,
         cursorColor: elevatedButtonColor,
         decoration: InputDecoration(
           focusedBorder: inputBorder(),
           border: inputBorder(),
-          labelText: ' Ady(ru) ',
+          labelText: ' Ady(tm) ',
         ),
-        validator: (validator) {
-          var l = validator.toString().length;
-          if (l < 1) return lang.nameRURequired;
-          return null;
-        },
+        validator: validatorFunc,
+        // validator: (validator) {
+        //   var l = validator.toString().length;
+        //   if (l < 1) return lang.nameTMRequired;
+        //   return null;
+        // },
       ),
     );
   }
