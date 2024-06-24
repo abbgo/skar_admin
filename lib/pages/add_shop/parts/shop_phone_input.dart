962ded53e1fbd24d33ea4e9b7ea2a49dfdfeca86
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-24 17:26:25 +0500

    feat: add ShopPhoneInput part widget

--- lib/pages/add_shop/parts/shop_phone_input.dart
+++ lib/pages/add_shop/parts/shop_phone_input.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:skar_admin/pages/parts/input_phone.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class PhoneNumberField extends StatelessWidget {
-  const PhoneNumberField({super.key, required this.ctrl});
+class ShopPhoneInput extends StatelessWidget {
+  const ShopPhoneInput({super.key, required this.ctrl});
 
   final TextEditingController ctrl;
 
