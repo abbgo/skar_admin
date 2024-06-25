9070c354efa11fa42cb235d33ca40621f8916e6c
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-25 12:27:05 +0500

    feat: add CoordinateInput part widget another file

---
+++ lib/pages/add_shop/parts/coordinate_input.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:skar_admin/pages/parts/input_part.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class CoordinateInput extends StatelessWidget {
+  const CoordinateInput({super.key, required this.ctrl, required this.label});
+
+  final TextEditingController ctrl;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
+    return Expanded(
+      child: InputPart(
+        ctrl: ctrl,
+        label: label,
+        keyboardType: TextInputType.number,
+        validatorFunc: (validator) {
+          var l = validator.toString().length;
+          if (l < 1) return lang.pleaseEnterThisInformation;
+          return null;
+        },
+      ),
+    );
+  }
+}
--- lib/pages/add_shop/parts/shop_coordinates_input.dart
+++ lib/pages/add_shop/parts/shop_coordinates_input.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:skar_admin/pages/parts/input_part.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_admin/pages/add_shop/parts/coordinate_input.dart';
 
 class ShopCoordinatesInput extends StatelessWidget {
   const ShopCoordinatesInput({
@@ -27,31 +26,6 @@ class ShopCoordinatesInput extends StatelessWidget {
           ],
         ),
       ],
-    );
-  }
-}
-
-class CoordinateInput extends StatelessWidget {
-  const CoordinateInput({super.key, required this.ctrl, required this.label});
-
-  final TextEditingController ctrl;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    var lang = AppLocalizations.of(context)!;
-
-    return Expanded(
-      child: InputPart(
-        ctrl: ctrl,
-        label: label,
-        keyboardType: TextInputType.number,
-        validatorFunc: (validator) {
-          var l = validator.toString().length;
-          if (l < 1) return lang.pleaseEnterThisInformation;
-          return null;
-        },
-      ),
     );
   }
 }
