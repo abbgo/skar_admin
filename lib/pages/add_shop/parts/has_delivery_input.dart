b11d35ea038823be991030037b47e32d3b1c0353
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-26 05:40:40 +0500

    fix: update HasDeliveryInput part widget

--- lib/pages/add_shop/parts/has_delivery_input.dart
+++ lib/pages/add_shop/parts/has_delivery_input.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/providers/pages/add_shop.dart';
+import 'package:skar_admin/styles/colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HasDeliveryInput extends ConsumerWidget {
   const HasDeliveryInput({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var lang = AppLocalizations.of(context)!;
     bool hasDelivery = ref.watch(hasDeliveryProvider);
     return Row(
       children: [
-        const Text('Eltip bermek hyzmaty bar: '),
+        Text('${lang.isThereDeliveryService} : '),
         Switch.adaptive(
+          activeColor: elevatedButtonColor,
           value: hasDelivery,
           onChanged: (value) =>
               ref.read(hasDeliveryProvider.notifier).state = value,
