d7542bba495fff085596dc48b6f46adeeec06630
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-27 06:39:34 +0500

    fix: update SelectImageBottomSheet part widget

--- lib/pages/parts/select_image_bottom_sheet.dart
+++ lib/pages/parts/select_image_bottom_sheet.dart
@@ -2,31 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:skar_admin/helpers/functions/file_upload.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SelectImageBottomSheet extends StatelessWidget {
   const SelectImageBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
-      child: const Wrap(
+      child: Wrap(
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ImageSourcePart(
                 icon: Icons.image,
-                text: 'Galery',
+                text: lang.galery,
                 imageSource: ImageSource.gallery,
               ),
               ImageSourcePart(
                 icon: Icons.camera,
-                text: 'Camera',
+                text: lang.camera,
                 imageSource: ImageSource.camera,
               ),
             ],
