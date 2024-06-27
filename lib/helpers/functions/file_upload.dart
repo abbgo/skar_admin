186a8fac86475ea2474e174808b8096b5bd8f8ef
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-27 07:02:35 +0500

    fix: update getImage function

--- lib/helpers/functions/file_upload.dart
+++ lib/helpers/functions/file_upload.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_size_getter/file_input.dart';
+import 'package:image_size_getter/image_size_getter.dart';
 import 'package:skar_admin/providers/pages/add_shop.dart';
 
 Future<void> getImage(WidgetRef ref, ImageSource imageSource) async {
-  final pickedFile = await ImagePicker().pickImage(source: imageSource);
+  XFile? pickedFile = await ImagePicker().pickImage(source: imageSource);
 
   if (pickedFile != null) {
+    final size = ImageSizeGetter.getSize(FileInput(File(pickedFile.path)));
+    print('--------------------- jpg = $size');
     ref.read(shopImageProvider.notifier).state = File(pickedFile.path);
   }
 
