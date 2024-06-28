fed8419ac031f9d0e47d8734358c9ba5c7ee966a
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-28 06:50:03 +0500

    feat: add loadSendImageProvider provider

--- lib/providers/parts/file_upload.dart
+++ lib/providers/parts/file_upload.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var isTrueImageProvider = StateProvider.autoDispose<bool>((ref) => true);
+var loadSendImageProvider = StateProvider.autoDispose<bool>((ref) => false);
