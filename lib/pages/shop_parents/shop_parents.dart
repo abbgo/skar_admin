fc4befa50ba7ddf5bf3208e982dadc2e668fa6c3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-01 18:28:18 +0500

    fix: update ShopParentsPage page

--- lib/pages/shop_parents/shop_parents.dart
+++ lib/pages/shop_parents/shop_parents.dart
@@ -4,7 +4,6 @@ import 'package:skar_admin/datas/static.dart';
 import 'package:skar_admin/helpers/methods/parts/input.dart';
 import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop.dart';
-import 'package:skar_admin/pages/parts/shop_list_tile/shop_list_tile.dart';
 import 'package:skar_admin/providers/api/shop.dart';
 import 'package:skar_admin/services/api/shop.dart';
 import 'package:skar_admin/styles/colors.dart';
@@ -33,38 +32,45 @@ class ShopParentsPage extends StatelessWidget {
       ),
       body: Consumer(
         builder: (context, ref, child) {
-          return ListView.builder(
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              final page = index ~/ pageSize + 1;
-              final indexInPage = index % pageSize;
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                final page = index ~/ pageSize + 1;
+                final indexInPage = index % pageSize;
 
-              ShopParams shopParams = ShopParams(
-                page: page,
-                isDeleted: false,
-                isShoppingCenter: true,
-              );
-              final AsyncValue<ResultShop> shops =
-                  ref.watch(fetchShopsProvider(shopParams));
+                ShopParams shopParams = ShopParams(
+                  page: page,
+                  isDeleted: false,
+                  isShoppingCenter: true,
+                );
+                final AsyncValue<ResultShop> shops =
+                    ref.watch(fetchShopsProvider(shopParams));
 
-              return shops.when(
-                skipLoadingOnRefresh: true,
-                skipLoadingOnReload: true,
-                skipError: true,
-                data: (response) {
-                  if (response.error != '') {
-                    return null;
-                  }
-                  if (indexInPage >= response.shops!.length) {
-                    return null;
-                  }
-                  Shop shop = response.shops![indexInPage];
-                  return ShopListTile(shop: shop);
-                },
-                error: (error, stackTrace) => errorMethod(error),
-                loading: () => null,
-              );
-            },
+                return shops.when(
+                  skipLoadingOnRefresh: true,
+                  skipLoadingOnReload: true,
+                  skipError: true,
+                  data: (response) {
+                    if (response.error != '') {
+                      return null;
+                    }
+                    if (indexInPage >= response.shops!.length) {
+                      return null;
+                    }
+                    Shop shop = response.shops![indexInPage];
+                    return Card(
+                      child: ListTile(
+                        title: Text(shop.nameTM),
+                      ),
+                    );
+                  },
+                  error: (error, stackTrace) => errorMethod(error),
+                  loading: () => null,
+                );
+              },
+            ),
           );
         },
       ),
