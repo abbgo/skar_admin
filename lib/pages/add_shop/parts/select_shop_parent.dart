e219397d9bc5b7a09c594b246df444d45600a085
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-01 12:32:25 +0500

    feat: add SelectShopParent part widget

---
+++ lib/pages/add_shop/parts/select_shop_parent.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectShopParent extends StatelessWidget {
+  const SelectShopParent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+            'Eger dukanynyz sowda merkezine degisli bolsa sowda merkezi saylan !'),
+      ],
+    );
+  }
+}
