94e23eeb846bd8859c013b714d7c748384dee720
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-07-02 06:15:35 +0500

    fix: update ResultShopParents part widget

--- lib/pages/shop_parents/parts/result_shop_parents.dart
+++ lib/pages/shop_parents/parts/result_shop_parents.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/datas/static.dart';
 import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop.dart';
+import 'package:skar_admin/pages/parts/no_result.dart';
 import 'package:skar_admin/providers/api/shop.dart';
 import 'package:skar_admin/services/api/shop.dart';
 
@@ -11,48 +12,49 @@ class ResultShopParents extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    String search = ref.watch(shopSearchProvider);
     bool hasShops = ref.watch(hasShopsProvider);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (context, index) {
-          final page = index ~/ pageSize + 1;
-          final indexInPage = index % pageSize;
+    return !hasShops
+        ? const NoResult()
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                final page = index ~/ pageSize + 1;
+                final indexInPage = index % pageSize;
 
-          ShopParams shopParams = ShopParams(
-            page: page,
-            isDeleted: false,
-            isShoppingCenter: true,
-          );
-          final AsyncValue<ResultShop> shops =
-              ref.watch(fetchShopsProvider(shopParams));
+                ShopParams shopParams = ShopParams(
+                  page: page,
+                  isDeleted: false,
+                  isShoppingCenter: true,
+                );
+                final AsyncValue<ResultShop> shops =
+                    ref.watch(fetchShopsProvider(shopParams));
 
-          return shops.when(
-            skipLoadingOnRefresh: true,
-            skipLoadingOnReload: true,
-            skipError: true,
-            data: (response) {
-              if (response.error != '') {
-                return null;
-              }
-              if (indexInPage >= response.shops!.length) {
-                return null;
-              }
-              Shop shop = response.shops![indexInPage];
-              return Card(
-                child: ListTile(
-                  title: Text(shop.nameTM),
-                ),
-              );
-            },
-            error: (error, stackTrace) => errorMethod(error),
-            loading: () => null,
+                return shops.when(
+                  skipLoadingOnRefresh: true,
+                  skipLoadingOnReload: true,
+                  skipError: true,
+                  data: (response) {
+                    if (response.error != '') {
+                      return null;
+                    }
+                    if (indexInPage >= response.shops!.length) {
+                      return null;
+                    }
+                    Shop shop = response.shops![indexInPage];
+                    return Card(
+                      child: ListTile(
+                        title: Text(shop.nameTM),
+                      ),
+                    );
+                  },
+                  error: (error, stackTrace) => errorMethod(error),
+                  loading: () => null,
+                );
+              },
+            ),
           );
-        },
-      ),
-    );
   }
 }
