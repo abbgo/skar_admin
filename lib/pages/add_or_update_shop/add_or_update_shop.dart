47d47c488c3c3c2ff32c7beb7f1a2562e52bc1f5
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-03 13:35:11 +0500

    feat: add AddOrUpdateShopPageBody part widget

--- lib/pages/add_or_update_shop/add_or_update_shop.dart
+++ lib/pages/add_or_update_shop/add_or_update_shop.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/has_shipping_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/select_shop_parent.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_address_ru_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_address_tm_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_coordinates_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_image_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_name_ru_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_name_tm_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_phone_2_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/shop_phone_input.dart';
-import 'package:skar_admin/pages/add_or_update_shop/parts/add_shop_button.dart';
-import 'package:skar_admin/pages/parts/cancel_button.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/add_or_update_shop_page_body.dart';
 import 'package:skar_admin/providers/api/shop.dart';
-import 'package:skar_admin/providers/pages/add_shop.dart';
 import 'package:skar_admin/services/api/shop.dart';
 
 class AddOrUpdateShopPage extends ConsumerStatefulWidget {
@@ -39,14 +26,6 @@ class AddOrUpdateShopPageState extends ConsumerState<AddOrUpdateShopPage> {
   final TextEditingController phone2Ctrl = TextEditingController();
   final TextEditingController latitudeCtrl = TextEditingController();
   final TextEditingController longitudeCtrl = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.shopID != '') {
-      fetchShopByID(widget.shopID);
-    }
-  }
 
   Future<void> fetchShopByID(String shopID) async {
     ShopParams params = ShopParams(shopID: shopID);
@@ -80,64 +59,21 @@ class AddOrUpdateShopPageState extends ConsumerState<AddOrUpdateShopPage> {
   @override
   Widget build(BuildContext context) {
     var lang = AppLocalizations.of(context)!;
-    bool loadCreateShop = ref.watch(loadCreateShopProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(lang.addShopInformation),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Form(
-              key: addShopformKey,
-              child: ListView(
-                physics: const BouncingScrollPhysics(),
-                children: [
-                  ShopNameTmInput(ctrl: nameTMCtrl),
-                  ShopNameRuInput(ctrl: nameRUCtrl),
-                  ShopAddressTmInput(ctrl: addressTMCtrl),
-                  ShopAddressRuInput(ctrl: addressRUCtrl),
-                  ShopPhoneInput(ctrl: phoneCtrl),
-                  ShopPhone2Input(ctrl: phone2Ctrl),
-                  SelectShopParent(
-                    latitudeCtrl: latitudeCtrl,
-                    longitudeCtrl: longitudeCtrl,
-                  ),
-                  ShopCoordinatesInput(
-                    latitudeCtrl: latitudeCtrl,
-                    longitudeCtrl: longitudeCtrl,
-                  ),
-                  const HasShippingInput(),
-                  const ShopImageInput(),
-                  const Divider(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const CancelButton(),
-                        AddShopButton(
-                          formKey: addShopformKey,
-                          nameTMCtrl: nameTMCtrl,
-                          nameRUCtrl: nameRUCtrl,
-                          addressTMCtrl: addressTMCtrl,
-                          addressRUCtrl: addressRUCtrl,
-                          phoneCtrl: phoneCtrl,
-                          phone2Ctrl: phone2Ctrl,
-                          latitudeCtrl: latitudeCtrl,
-                          longitudeCtrl: longitudeCtrl,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          loadCreateShop ? loadProcess : const SizedBox(),
-        ],
+      body: AddOrUpdateShopPageBody(
+        addShopformKey: addShopformKey,
+        nameTMCtrl: nameTMCtrl,
+        nameRUCtrl: nameRUCtrl,
+        addressTMCtrl: addressTMCtrl,
+        addressRUCtrl: addressRUCtrl,
+        phoneCtrl: phoneCtrl,
+        phone2Ctrl: phone2Ctrl,
+        latitudeCtrl: latitudeCtrl,
+        longitudeCtrl: longitudeCtrl,
       ),
     );
   }
---
+++ lib/pages/add_or_update_shop/parts/add_or_update_shop_page_body.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/helpers/static_data.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/add_shop_button.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/has_shipping_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/select_shop_parent.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_address_ru_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_address_tm_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_coordinates_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_image_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_name_ru_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_name_tm_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_phone_2_input.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/shop_phone_input.dart';
+import 'package:skar_admin/pages/parts/cancel_button.dart';
+import 'package:skar_admin/providers/pages/add_shop.dart';
+
+class AddOrUpdateShopPageBody extends StatelessWidget {
+  const AddOrUpdateShopPageBody({
+    super.key,
+    required this.addShopformKey,
+    required this.nameTMCtrl,
+    required this.nameRUCtrl,
+    required this.addressTMCtrl,
+    required this.addressRUCtrl,
+    required this.phoneCtrl,
+    required this.phone2Ctrl,
+    required this.latitudeCtrl,
+    required this.longitudeCtrl,
+  });
+
+  final GlobalKey<FormState> addShopformKey;
+  final TextEditingController nameTMCtrl;
+  final TextEditingController nameRUCtrl;
+  final TextEditingController addressTMCtrl;
+  final TextEditingController addressRUCtrl;
+  final TextEditingController phoneCtrl;
+  final TextEditingController phone2Ctrl;
+  final TextEditingController latitudeCtrl;
+  final TextEditingController longitudeCtrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Form(
+            key: addShopformKey,
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              children: [
+                ShopNameTmInput(ctrl: nameTMCtrl),
+                ShopNameRuInput(ctrl: nameRUCtrl),
+                ShopAddressTmInput(ctrl: addressTMCtrl),
+                ShopAddressRuInput(ctrl: addressRUCtrl),
+                ShopPhoneInput(ctrl: phoneCtrl),
+                ShopPhone2Input(ctrl: phone2Ctrl),
+                SelectShopParent(
+                  latitudeCtrl: latitudeCtrl,
+                  longitudeCtrl: longitudeCtrl,
+                ),
+                ShopCoordinatesInput(
+                  latitudeCtrl: latitudeCtrl,
+                  longitudeCtrl: longitudeCtrl,
+                ),
+                const HasShippingInput(),
+                const ShopImageInput(),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const CancelButton(),
+                      AddShopButton(
+                        formKey: addShopformKey,
+                        nameTMCtrl: nameTMCtrl,
+                        nameRUCtrl: nameRUCtrl,
+                        addressTMCtrl: addressTMCtrl,
+                        addressRUCtrl: addressRUCtrl,
+                        phoneCtrl: phoneCtrl,
+                        phone2Ctrl: phone2Ctrl,
+                        latitudeCtrl: latitudeCtrl,
+                        longitudeCtrl: longitudeCtrl,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Consumer(
+          builder: (context, ref, child) {
+            bool loadCreateShop = ref.watch(loadCreateShopProvider);
+            return loadCreateShop ? loadProcess : const SizedBox();
+          },
+        ),
+      ],
+    );
+  }
+}
