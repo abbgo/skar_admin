99f79d17fbe96452e943be50277d9875e9a5acc2
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-03 13:52:15 +0500

    fix: update AddOrUpdateShopPage page

--- lib/pages/add_or_update_shop/add_or_update_shop.dart
+++ lib/pages/add_or_update_shop/add_or_update_shop.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/helpers/static_data.dart';
 import 'package:skar_admin/models/shop.dart';
 import 'package:skar_admin/pages/add_or_update_shop/parts/add_or_update_shop_page_body.dart';
+import 'package:skar_admin/pages/parts/no_result.dart';
 import 'package:skar_admin/providers/api/shop.dart';
 import 'package:skar_admin/services/api/shop.dart';
 
-class AddOrUpdateShopPage extends ConsumerStatefulWidget {
+class AddOrUpdateShopPage extends StatefulWidget {
   const AddOrUpdateShopPage({super.key, required this.shopID});
 
   final String shopID;
 
   @override
-  // State<AddOrUpdateShopPage> createState() => _AddOrUpdateShopPageState();
-  AddOrUpdateShopPageState createState() => AddOrUpdateShopPageState();
+  State<AddOrUpdateShopPage> createState() => _AddOrUpdateShopPageState();
 }
 
-class AddOrUpdateShopPageState extends ConsumerState<AddOrUpdateShopPage> {
+class _AddOrUpdateShopPageState extends State<AddOrUpdateShopPage> {
   final GlobalKey<FormState> addShopformKey = GlobalKey<FormState>();
   final TextEditingController nameTMCtrl = TextEditingController();
   final TextEditingController nameRUCtrl = TextEditingController();
@@ -26,21 +27,6 @@ class AddOrUpdateShopPageState extends ConsumerState<AddOrUpdateShopPage> {
   final TextEditingController phone2Ctrl = TextEditingController();
   final TextEditingController latitudeCtrl = TextEditingController();
   final TextEditingController longitudeCtrl = TextEditingController();
-
-  Future<void> fetchShopByID(String shopID) async {
-    ShopParams params = ShopParams(shopID: shopID);
-    ResultShop resultShop = await ref.watch(fetchShopProvider(params).future);
-    Shop shop = resultShop.shop!;
-
-    nameTMCtrl.text = shop.nameTM;
-    nameRUCtrl.text = shop.nameRU;
-    addressTMCtrl.text = shop.addressTM!;
-    addressRUCtrl.text = shop.addressRU!;
-    phoneCtrl.text = shop.phones![0];
-    phone2Ctrl.text = shop.phones![1];
-    latitudeCtrl.text = shop.latitude!.toString();
-    longitudeCtrl.text = shop.longitude!.toString();
-  }
 
   @override
   void dispose() {
@@ -60,21 +46,58 @@ class AddOrUpdateShopPageState extends ConsumerState<AddOrUpdateShopPage> {
   Widget build(BuildContext context) {
     var lang = AppLocalizations.of(context)!;
 
+    AddOrUpdateShopPageBody addOrUpdateShopPageBody = AddOrUpdateShopPageBody(
+      addShopformKey: addShopformKey,
+      nameTMCtrl: nameTMCtrl,
+      nameRUCtrl: nameRUCtrl,
+      addressTMCtrl: addressTMCtrl,
+      addressRUCtrl: addressRUCtrl,
+      phoneCtrl: phoneCtrl,
+      phone2Ctrl: phone2Ctrl,
+      latitudeCtrl: latitudeCtrl,
+      longitudeCtrl: longitudeCtrl,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(lang.addShopInformation),
       ),
-      body: AddOrUpdateShopPageBody(
-        addShopformKey: addShopformKey,
-        nameTMCtrl: nameTMCtrl,
-        nameRUCtrl: nameRUCtrl,
-        addressTMCtrl: addressTMCtrl,
-        addressRUCtrl: addressRUCtrl,
-        phoneCtrl: phoneCtrl,
-        phone2Ctrl: phone2Ctrl,
-        latitudeCtrl: latitudeCtrl,
-        longitudeCtrl: longitudeCtrl,
-      ),
+      body: widget.shopID == ''
+          ? addOrUpdateShopPageBody
+          : Consumer(
+              builder: (context, ref, child) {
+                ShopParams params = ShopParams(shopID: widget.shopID);
+                final AsyncValue<ResultShop> resultShop =
+                    ref.watch(fetchShopProvider(params));
+
+                return resultShop.when(
+                  skipLoadingOnRefresh: true,
+                  skipLoadingOnReload: true,
+                  skipError: true,
+                  data: (response) {
+                    if (response.error != '' || response.shop == null) {
+                      return const NoResult();
+                    }
+
+                    Shop shop = response.shop!;
+                    nameTMCtrl.text = shop.nameTM;
+                    nameRUCtrl.text = shop.nameRU;
+                    addressTMCtrl.text = shop.addressTM!;
+                    addressRUCtrl.text = shop.addressRU!;
+                    phoneCtrl.text =
+                        shop.phones![0].substring(shop.phones![0].length - 8);
+                    phone2Ctrl.text =
+                        shop.phones![1].substring(shop.phones![1].length - 8);
+                    latitudeCtrl.text = shop.latitude!.toString();
+                    longitudeCtrl.text = shop.longitude!.toString();
+
+                    return addOrUpdateShopPageBody;
+                  },
+                  error: (error, stackTrace) => errorMethod(error),
+                  loading: () => loadWidget,
+                );
+              },
+            ),
     );
   }
 }
