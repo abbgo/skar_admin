f2fd26134212db78fc2644599dfa779579043c5f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-04 11:13:53 +0500

    feat: add fetchProducts function

--- lib/services/api/product.dart
+++ lib/services/api/product.dart
@@ -1,1 +1,54 @@
-class ProductApiService {}
+import 'dart:convert';
+
+import 'package:skar_admin/helpers/functions/static_data.dart';
+import 'package:skar_admin/helpers/static_data.dart';
+import 'package:skar_admin/models/product.dart';
+import 'package:http/http.dart' as http;
+
+class ProductApiService {
+  // fetch products -------------------------------------------------------
+  Future<ResultProduct> fetchProducts({
+    required String accessToken,
+    required int page,
+    required String shopID,
+    required bool isDeleted,
+    required String search,
+    required String lang,
+  }) async {
+    Uri uri = Uri.parse('$apiUrl/back/shops').replace(
+      queryParameters: {
+        'limit': '10',
+        'page': '$page',
+        'shop_id': shopID,
+        'is_deleted': '$isDeleted',
+        'search': search,
+        'lang': lang,
+      },
+    );
+
+    try {
+      http.Response response = await http.get(
+        uri,
+        headers: tokenHeader(accessToken),
+      );
+      var jsonData = json.decode(response.body);
+
+      if (response.statusCode == 200 && jsonData['status']) {
+        if (jsonData['products'] == null) {
+          return const ResultProduct(products: [], error: '');
+        }
+
+        var productsList = jsonData['products'] as List;
+        return ResultProduct(
+          products: productsList
+              .map<Product>((propJson) => Product.fromJson(propJson))
+              .toList(),
+          error: '',
+        );
+      }
+      return const ResultProduct(products: [], error: 'auth error');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
