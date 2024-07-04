fd05d15ddb92223358efa72d387899994e74a6d4
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-04 11:20:38 +0500

    feat: add fetchProductsProvider provider

--- lib/providers/api/product.dart
+++ lib/providers/api/product.dart
@@ -1,5 +1,36 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/product.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
 import 'package:skar_admin/services/api/product.dart';
 
 final productApiProvider =
     Provider<ProductApiService>((ref) => ProductApiService());
+
+var fetchProductsProvider =
+    FutureProvider.autoDispose.family<ResultProduct, ProductParams>(
+  (ref, arg) async {
+    ResultProduct result = ResultProduct.defaultResult();
+
+    try {
+      String accessToken = await ref.read(accessTokenProvider);
+      ResultProduct resultProduct =
+          await ref.read(productApiProvider).fetchProducts(
+                accessToken: accessToken,
+                page: arg.page!,
+                isDeleted: arg.isDeleted!,
+                search: '',
+                shopID: '',
+                lang: '',
+              );
+
+      if (resultProduct.error == 'auth error') {
+        await ref.read(accessTokenProvider.notifier).update('');
+      }
+
+      result = resultProduct;
+    } catch (e) {
+      result = ResultProduct(error: e.toString());
+    }
+    return result;
+  },
+);
