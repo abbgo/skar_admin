c534ede79296e668a25972c40c69a90538a7c03c
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 12:08:31 +0500

    feat: add hasBrendProvider and brendSearchProvider provides

--- lib/pages/brends/brends.dart
+++ lib/pages/brends/brends.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/pages/brends/parts/result_brends.dart';
 import 'package:skar_admin/pages/parts/search_input.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_admin/providers/pages/brend.dart';
 
 class BrendsPage extends StatelessWidget {
   const BrendsPage({super.key});
@@ -10,7 +12,17 @@ class BrendsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: SearchInput(label: AppLocalizations.of(context)!.searchBrand),
+        title: Consumer(
+          builder: (context, ref, child) {
+            return SearchInput(
+              label: AppLocalizations.of(context)!.searchBrand,
+              onSubmitted: (value) {
+                ref.read(brendSearchProvider.notifier).state = value;
+                ref.read(hasBrendProvider.notifier).state = true;
+              },
+            );
+          },
+        ),
       ),
       body: const ResultBrends(),
     );
---
+++ lib/providers/pages/brend.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+var brendSearchProvider = StateProvider.autoDispose<String>((ref) => '');
+var hasBrendProvider = StateProvider.autoDispose<bool>((ref) => true);
