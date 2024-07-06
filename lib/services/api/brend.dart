245bae660aa706ed74d429133c312033ebcf9e4a
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 11:35:18 +0500

    feat: add BrendApiService service class and add fetchBrends function

--- lib/services/api/brend.dart
+++ lib/services/api/brend.dart
@@ -1,3 +1,53 @@
+import 'dart:convert';
+
+import 'package:skar_admin/helpers/functions/static_data.dart';
+import 'package:skar_admin/helpers/static_data.dart';
+import 'package:skar_admin/models/brend.dart';
+import 'package:http/http.dart' as http;
+
 class BrendApiService {
   // fetch brends
+  Future<ResultBrend> fetchBrends({
+    required String accessToken,
+    required int page,
+    required String shopID,
+    required bool isDeleted,
+    required String search,
+    required String lang,
+  }) async {
+    Uri uri = Uri.parse('$apiUrl/back/brends').replace(
+      queryParameters: {
+        'limit': '10',
+        'page': '$page',
+        'is_deleted': '$isDeleted',
+        'search': search,
+        'lang': lang,
+      },
+    );
+
+    try {
+      http.Response response = await http.get(
+        uri,
+        headers: tokenHeader(accessToken),
+      );
+      var jsonData = json.decode(response.body);
+
+      if (response.statusCode == 200 && jsonData['status']) {
+        if (jsonData['products'] == null) {
+          return const ResultBrend(brends: [], error: '');
+        }
+
+        var brendsList = jsonData['brends'] as List;
+        return ResultBrend(
+          brends: brendsList
+              .map<Brend>((propJson) => Brend.fromJson(propJson))
+              .toList(),
+          error: '',
+        );
+      }
+      return const ResultBrend(brends: [], error: 'auth error');
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
