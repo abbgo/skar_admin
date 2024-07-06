4f2f129ac6aef1857ce618a4bdad705905ab2be4
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 14:50:19 +0500

    feat: add ShowParentShop part widget

--- lib/pages/add_or_update_shop/parts/select_shop_parent.dart
+++ lib/pages/add_or_update_shop/parts/select_shop_parent.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/helpers/methods/navigation.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:skar_admin/models/shop.dart';
+import 'package:skar_admin/pages/add_or_update_shop/parts/show_parent_shop.dart';
 import 'package:skar_admin/pages/shopping_center/shopping_center.dart';
-import 'package:skar_admin/providers/local_storadge/setting.dart';
-import 'package:skar_admin/providers/pages/shopping_center.dart';
 
 class SelectShopParent extends StatelessWidget {
   const SelectShopParent({
     super.key,
     required this.latitudeCtrl,
     required this.longitudeCtrl,
+    this.shopID,
   });
 
   final TextEditingController latitudeCtrl;
   final TextEditingController longitudeCtrl;
+
+  final String? shopID;
 
   @override
   Widget build(BuildContext context) {
@@ -30,26 +30,7 @@ class SelectShopParent extends StatelessWidget {
             '${lang.ifYourStoreIsLocatedInsideMallSelectMall} :',
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-          Consumer(
-            builder: (context, ref, child) {
-              SelectedShop selectedShop =
-                  ref.watch(selectedShoppincCenterProvider);
-              bool isTM = ref.watch(langProvider) == 'tr';
-
-              return selectedShop == SelectedShop.defaultSelectedShop()
-                  ? const SizedBox()
-                  : Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        isTM ? selectedShop.nameTM : selectedShop.nameRU,
-                        style: const TextStyle(
-                          color: Colors.green,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    );
-            },
-          ),
+          ShowParentShop(shopID: shopID),
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
---
+++ lib/pages/add_or_update_shop/parts/show_parent_shop.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/shop.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
+import 'package:skar_admin/providers/pages/shopping_center.dart';
+
+class ShowParentShop extends ConsumerWidget {
+  const ShowParentShop({super.key, this.shopID});
+
+  final String? shopID;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    SelectedShop selectedShop = ref.watch(selectedShoppincCenterProvider);
+    bool isTM = ref.watch(langProvider) == 'tr';
+
+    return selectedShop == SelectedShop.defaultSelectedShop()
+        ? const SizedBox()
+        : Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              isTM ? selectedShop.nameTM : selectedShop.nameRU,
+              style: const TextStyle(
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+  }
+}
