b2f70d8c0b85a6f904ee59e4dc50f98bd6487865
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-07-06 22:13:01 +0500

    feat: add fetchCategoriesProvider provider

--- lib/providers/api/category.dart
+++ lib/providers/api/category.dart
@@ -1,5 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/category.dart';
 import 'package:skar_admin/services/api/category.dart';
 
 final categoryApiProvider =
     Provider<CategoryApiService>((ref) => CategoryApiService());
+
+var fetchCategoriesProvider = FutureProvider.autoDispose<ResultCategory>(
+  (ref) async {
+    ResultCategory result = ResultCategory.defaultResult();
+
+    try {
+      ResultCategory resultBrend =
+          await ref.read(categoryApiProvider).fetchCategories();
+
+      result = resultBrend;
+    } catch (e) {
+      result = ResultCategory(error: e.toString());
+    }
+    return result;
+  },
+);
