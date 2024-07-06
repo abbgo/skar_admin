91c40480bb1c30fbd40499c6fb5a238e7a0b85df
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 16:04:30 +0500

    feat: add SelectProductCategories part widget

---
+++ lib/pages/add_or_update_product/parts/select_product_categories.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SelectProductCategories extends StatelessWidget {
+  const SelectProductCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Select categories'),
+      ),
+    );
+  }
+}
