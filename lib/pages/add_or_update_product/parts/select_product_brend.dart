f5cae59b7137e4c0b0da796ee0a7d25b1704dedb
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 12:41:48 +0500

    fix: update SelectProductBrend part widget

--- lib/pages/add_or_update_product/parts/select_product_brend.dart
+++ lib/pages/add_or_update_product/parts/select_product_brend.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/helpers/methods/navigation.dart';
+import 'package:skar_admin/models/brend.dart';
 import 'package:skar_admin/pages/brends/brends.dart';
+import 'package:skar_admin/providers/pages/brend.dart';
 
 class SelectProductBrend extends StatelessWidget {
   const SelectProductBrend({super.key});
@@ -15,9 +18,30 @@ class SelectProductBrend extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            '${lang.enterTheProductBrand} :',
-            style: const TextStyle(fontWeight: FontWeight.bold),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                '${lang.enterTheProductBrand} : ',
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Consumer(
+                builder: (context, ref, child) {
+                  Brend selectedBrend = ref.watch(selectedBrendProvider);
+
+                  return Text(
+                    selectedBrend.name,
+                    softWrap: true,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
+              )
+            ],
           ),
           SizedBox(
             width: double.infinity,
