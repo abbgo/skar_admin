9ceba761b25581647d048d568cb37e517a12693d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 16:16:59 +0500

    fix: update CategoriesPage

--- lib/pages/categories/categories.dart
+++ lib/pages/categories/categories.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/pages/parts/search_input.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Consumer(
+          builder: (context, ref, child) {
+            return SearchInput(
+              label: AppLocalizations.of(context)!.searchCategory,
+            );
+          },
+        ),
+      ),
+      body: const Center(
         child: Text('Categories'),
       ),
     );
