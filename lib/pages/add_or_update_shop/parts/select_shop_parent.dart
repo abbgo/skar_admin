bb2dafc18463a54c8ec1ab21b4505f853b01fe9c
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-06 12:51:12 +0500

    fix: update SelectShopParent part widget

--- lib/pages/add_or_update_shop/parts/select_shop_parent.dart
+++ lib/pages/add_or_update_shop/parts/select_shop_parent.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/helpers/methods/navigation.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_admin/models/shop.dart';
 import 'package:skar_admin/pages/shopping_center/shopping_center.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
+import 'package:skar_admin/providers/pages/shopping_center.dart';
 
 class SelectShopParent extends StatelessWidget {
   const SelectShopParent({
@@ -23,8 +27,28 @@ class SelectShopParent extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            lang.ifYourStoreIsLocatedInsideMallSelectMall,
+            '${lang.ifYourStoreIsLocatedInsideMallSelectMall} :',
             style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              SelectedShop selectedShop =
+                  ref.watch(selectedShoppincCenterProvider);
+              bool isTM = ref.watch(langProvider) == 'tr';
+
+              return selectedShop == SelectedShop.defaultSelectedShop()
+                  ? const SizedBox()
+                  : Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Text(
+                        isTM ? selectedShop.nameTM : selectedShop.nameRU,
+                        style: const TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+            },
           ),
           SizedBox(
             width: double.infinity,
