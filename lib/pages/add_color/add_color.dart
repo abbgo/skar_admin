46a5c7e8990efd5b0e7b711c9ba5b62f3381f275
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 17:27:27 +0500

    fix: update AddColorPage

--- lib/pages/add_color/add_color.dart
+++ lib/pages/add_color/add_color.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/pages/add_or_update_shop/add_or_update_shop.dart';
 import 'package:skar_admin/pages/parts/floating_button.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AddColorPage extends StatelessWidget {
   const AddColorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(centerTitle: false, title: Text(lang.productTypes)),
       floatingActionButton: const FloatingButton(
         page: AddOrUpdateShopPage(shopID: ''),
         text: 'Gosnus gos',
