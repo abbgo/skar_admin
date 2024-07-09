794bef0694b41eea1d3eda1a036501d141804be5
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 16:09:15 +0500

    fix: update ProductPriceInput part widget

--- lib/pages/add_or_update_product/parts/product_price_input.dart
+++ lib/pages/add_or_update_product/parts/product_price_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/helpers/functions/validation.dart';
 import 'package:skar_admin/pages/parts/input_part.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProductPriceInput extends StatelessWidget {
   const ProductPriceInput({super.key, required this.ctrl});
@@ -12,7 +13,7 @@ class ProductPriceInput extends StatelessWidget {
     return Expanded(
       child: InputPart(
         ctrl: ctrl,
-        label: ' Bahasy * ',
+        label: ' ${AppLocalizations.of(context)!.price} * ',
         validatorFunc: (validator) => textInputValidate(validator, context),
       ),
     );
