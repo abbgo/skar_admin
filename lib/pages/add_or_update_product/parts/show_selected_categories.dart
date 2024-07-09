e17bcd19cc49523c2e7865b8f90339816cca4003
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 12:46:04 +0500

    fix: update ShowSelectedCategories part widget

--- lib/pages/add_or_update_product/parts/show_selected_categories.dart
+++ lib/pages/add_or_update_product/parts/show_selected_categories.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/category.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
 import 'package:skar_admin/providers/pages/category.dart';
 
 class ShowSelectedCategories extends ConsumerWidget {
@@ -9,6 +10,7 @@ class ShowSelectedCategories extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     List<Category> selectedCategories = ref.watch(selectedCategoriesProvider);
+    bool isTM = ref.watch(langProvider) == 'tr';
 
     return selectedCategories.isNotEmpty
         ? ListView.builder(
@@ -16,7 +18,15 @@ class ShowSelectedCategories extends ConsumerWidget {
             itemCount: selectedCategories.length,
             itemBuilder: (context, index) {
               Category category = selectedCategories[index];
-              return Text(category.nameTM);
+              return Text(
+                isTM
+                    ? '${index + 1}. ${category.nameTM}'
+                    : '${index + 1}. ${category.nameRU}',
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
             },
           )
         : const SizedBox();
