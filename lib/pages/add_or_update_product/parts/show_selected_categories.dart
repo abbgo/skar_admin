ffc4476b77c43a40472f9cf02793e26a1d03e59b
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 12:09:00 +0500

    feat: add ShowSelectedCategories part widget

---
+++ lib/pages/add_or_update_product/parts/show_selected_categories.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/category.dart';
+import 'package:skar_admin/providers/pages/category.dart';
+
+class ShowSelectedCategories extends ConsumerWidget {
+  const ShowSelectedCategories({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Category> selectedCategories = ref.watch(selectedCategoriesProvider);
+
+    return ListView.builder(
+      itemCount: selectedCategories.length,
+      itemBuilder: (context, index) {
+        Category category = selectedCategories[index];
+        return Text(category.nameTM);
+      },
+    );
+  }
+}
