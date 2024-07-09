2c3f91b9100183487d9cb8da9d068b5024d3b5e7
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 17:45:39 +0500

    feat: add ProductColorNameInput part widget and update AddProductColorPage

--- lib/pages/add_product_color/add_product_color.dart
+++ lib/pages/add_product_color/add_product_color.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:skar_admin/pages/add_product_color/parts/product_color_name_input.dart';
 
-class AddProductColorPage extends StatelessWidget {
+class AddProductColorPage extends StatefulWidget {
   const AddProductColorPage({super.key});
+
+  @override
+  State<AddProductColorPage> createState() => _AddProductColorPageState();
+}
+
+class _AddProductColorPageState extends State<AddProductColorPage> {
+  final GlobalKey<FormState> productColorFormKey = GlobalKey<FormState>();
+  final TextEditingController nameCtrl = TextEditingController();
+
+  @override
+  void dispose() {
+    nameCtrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Haryt gornusini gosmak'),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          ProductColorNameInput(ctrl: nameCtrl),
+        ],
       ),
     );
   }
---
+++ lib/pages/add_product_color/parts/product_color_name_input.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:skar_admin/helpers/functions/validation.dart';
+import 'package:skar_admin/pages/parts/input_part.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ProductColorNameInput extends StatelessWidget {
+  const ProductColorNameInput({super.key, required this.ctrl});
+
+  final TextEditingController ctrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputPart(
+      ctrl: ctrl,
+      label: ' ${AppLocalizations.of(context)!.name} (tm) *',
+      validatorFunc: (validator) => textInputValidate(validator, context),
+    );
+  }
+}
