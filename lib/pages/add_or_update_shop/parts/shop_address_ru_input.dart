b8260ff067b05cfe3f6f4cdfb9625c4c61f4cf4f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-09 16:02:50 +0500

    fix: update ShopAddressRuInput and ShopAddressTmInput part widgets

--- lib/pages/add_or_update_shop/parts/shop_address_ru_input.dart
+++ lib/pages/add_or_update_shop/parts/shop_address_ru_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/helpers/functions/validation.dart';
 import 'package:skar_admin/pages/parts/input_part.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShopAddressRuInput extends StatelessWidget {
   const ShopAddressRuInput({super.key, required this.ctrl});
@@ -11,7 +12,7 @@ class ShopAddressRuInput extends StatelessWidget {
   Widget build(BuildContext context) {
     return InputPart(
       ctrl: ctrl,
-      label: 'Salgysy (ru) *',
+      label: '${AppLocalizations.of(context)!.address} (ru) *',
       maxLines: 3,
       validatorFunc: (validator) => textInputValidate(validator, context),
     );
--- lib/pages/add_or_update_shop/parts/shop_address_tm_input.dart
+++ lib/pages/add_or_update_shop/parts/shop_address_tm_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/helpers/functions/validation.dart';
 import 'package:skar_admin/pages/parts/input_part.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShopAddressTmInput extends StatelessWidget {
   const ShopAddressTmInput({super.key, required this.ctrl});
@@ -11,7 +12,7 @@ class ShopAddressTmInput extends StatelessWidget {
   Widget build(BuildContext context) {
     return InputPart(
       ctrl: ctrl,
-      label: 'Salgysy (tm) *',
+      label: '${AppLocalizations.of(context)!.address} (tm) *',
       maxLines: 3,
       validatorFunc: (validator) => textInputValidate(validator, context),
     );
