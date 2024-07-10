c932d8435704e51b6081c4c964aa8f6200ff74ea
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 11:42:13 +0500

    fix: update DimensionGroupsWithDimensions part widget

--- lib/pages/dimension_groups_with_dimensions/dimension_groups_with_dimensions.dart
+++ lib/pages/dimension_groups_with_dimensions/dimension_groups_with_dimensions.dart
@@ -1,11 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/datas/static.dart';
+import 'package:skar_admin/helpers/static_data.dart';
+import 'package:skar_admin/models/dimension.dart';
+import 'package:skar_admin/models/dimension_group.dart';
+import 'package:skar_admin/providers/api/dimension_group.dart';
+import 'package:skar_admin/services/api/brend.dart';
 
 class DimensionGroupsWithDimensions extends ConsumerWidget {
   const DimensionGroupsWithDimensions({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text('some');
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final page = index ~/ pageSize + 1;
+        final indexInPage = index % pageSize;
+
+        DefaultParams params = DefaultParams(page: page, isDeleted: false);
+        final AsyncValue<ResultDimensionGroup> dimensionGroups =
+            ref.watch(fetchDimensionGroupsWithDimensionsProvider(params));
+
+        return dimensionGroups.when(
+          skipLoadingOnRefresh: true,
+          skipLoadingOnReload: true,
+          skipError: true,
+          data: (response) {
+            if (response.error != '') {
+              return null;
+            }
+            if (indexInPage >= response.dimensionGroups!.length) {
+              return null;
+            }
+
+            DimensionGroup dimensionGroup =
+                response.dimensionGroups![indexInPage];
+            List<Dimension> dimensions = dimensionGroup.dimensions!;
+
+            return Card(
+              child: ExpansionTile(
+                title: Text(dimensionGroup.name),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                        crossAxisSpacing: 2,
+                        mainAxisSpacing: 10,
+                        mainAxisExtent: 40,
+                      ),
+                      itemCount: dimensions.length,
+                      itemBuilder: (context, index) {
+                        return ElevatedButton(
+                          onPressed: () {},
+                          child: Text(dimensions[index].dimension),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          error: (error, stackTrace) => errorMethod(error),
+          loading: () => null,
+        );
+      },
+    );
   }
 }
