b2a7ec2d6a825f356a9cdf76137110ab57760467
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 12:50:00 +0500

    fix: update DimensionButton

--- lib/pages/dimension_groups_with_dimensions/parts/dimension_button.dart
+++ lib/pages/dimension_groups_with_dimensions/parts/dimension_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/dimension.dart';
+import 'package:skar_admin/providers/pages/add_or_update_product.dart';
+import 'package:skar_admin/styles/colors.dart';
 
 class DimensionButton extends ConsumerWidget {
   const DimensionButton({super.key, required this.d});
@@ -9,9 +11,35 @@ class DimensionButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ElevatedButton(
-      onPressed: () async {},
-      child: Text(d.dimension),
+    List<Dimension> selectedDimensions = ref.watch(selectedDimensionsProvider);
+
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: elevatedButtonColor),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              !selectedDimensions.contains(d) ? Colors.white : null,
+        ),
+        onPressed: () async {
+          if (selectedDimensions.contains(d)) {
+            await ref
+                .read(selectedDimensionsProvider.notifier)
+                .removeDimension(d);
+            return;
+          }
+
+          await ref.read(selectedDimensionsProvider.notifier).addDimension(d);
+        },
+        child: Text(
+          d.dimension,
+          style: TextStyle(
+            color: !selectedDimensions.contains(d) ? elevatedButtonColor : null,
+          ),
+        ),
+      ),
     );
   }
 }
