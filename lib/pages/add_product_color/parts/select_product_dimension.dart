dc99e66e8e8c9c4b04246f1e7b8bbc7810869eed
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 12:17:41 +0500

    fix: update SelectProductDimension

--- lib/pages/add_product_color/parts/select_product_dimension.dart
+++ lib/pages/add_product_color/parts/select_product_dimension.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:skar_admin/pages/dimension_groups_with_dimensions/dimension_groups_with_dimensions.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SelectProductDimension extends StatelessWidget {
   const SelectProductDimension({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Harydyn bu renkinin olceglerini saylan: '),
-        DimensionGroupsWithDimensions(),
+        Text(
+          '${AppLocalizations.of(context)!.selectProductColorDimensions} * : ',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const DimensionGroupsWithDimensions(),
       ],
     );
   }
