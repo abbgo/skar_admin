16ff34f5273ea059a16dda468d49ac6951d82580
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 16:22:22 +0500

    feat: add removeBackgroundProvider provider

--- lib/providers/parts/file_upload.dart
+++ lib/providers/parts/file_upload.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var loadSendImageProvider = StateProvider.autoDispose<bool>((ref) => false);
+var removeBackgroundProvider = StateProvider.autoDispose<bool>((ref) => false);
