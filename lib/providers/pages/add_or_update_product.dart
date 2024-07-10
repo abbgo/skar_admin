d2542f86e8d393c1c8378d1bd2a6220b9d2f1dc9
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 15:30:41 +0500

    feat: add productImageProvider and productImagePathProvider providers

--- lib/providers/pages/add_or_update_product.dart
+++ lib/providers/pages/add_or_update_product.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/dimension.dart';
 
 final loadCreateProductProvider =
     StateProvider.autoDispose<bool>((ref) => false);
 final visibleProductProvider = StateProvider<bool>((ref) => true);
+final productImageProvider = StateProvider.autoDispose<File?>((ref) => null);
+final productImagePathProvider = StateProvider<String>((ref) => '');
 
 class SelectedDimensionsNotifier extends StateNotifier<List<Dimension>> {
   SelectedDimensionsNotifier() : super([]);
