621de9e38668121e11ef211972179afe0a6601bc
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 12:30:02 +0500

    feat: add SelectedDimensionsNotifier notifier class and add addDimension and add removeDimension functions

--- lib/providers/pages/add_or_update_product.dart
+++ lib/providers/pages/add_or_update_product.dart
@@ -1,5 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/dimension.dart';
 
 final loadCreateProductProvider =
     StateProvider.autoDispose<bool>((ref) => false);
 final visibleProductProvider = StateProvider<bool>((ref) => true);
+
+class SelectedDimensionsNotifier extends StateNotifier<List<Dimension>> {
+  SelectedDimensionsNotifier() : super([]);
+
+  Future<void> addDimension(Dimension dimension) async {
+    if (!state.contains(dimension)) {
+      state = [...state, dimension];
+    }
+  }
+
+  Future<void> removeDimension(Dimension dimension) async {
+    state = state.where((i) => i != dimension).toList();
+  }
+}
