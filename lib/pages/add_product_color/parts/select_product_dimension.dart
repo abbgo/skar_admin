09f4a0d5c394cfe910b0e6b73ea7e7ee4f1fb61b
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-10 11:24:41 +0500

    feat: add DimensionGroupsWithDimensions page and update SelectProductDimension part widget

--- lib/pages/add_product_color/parts/select_product_dimension.dart
+++ lib/pages/add_product_color/parts/select_product_dimension.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:skar_admin/pages/dimension_groups_with_dimensions/dimension_groups_with_dimensions.dart';
 
 class SelectProductDimension extends StatelessWidget {
   const SelectProductDimension({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Harydyn bu renkinin olceglerini saylan: '),
+        DimensionGroupsWithDimensions(),
+      ],
+    );
   }
 }
