2728532a6eefc343433b27b87d28adc570113097
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 14:19:47 +0500

    fix: update ProductColorCard part widget

--- lib/pages/product_colors/parts/product_color_card.dart
+++ lib/pages/product_colors/parts/product_color_card.dart
@@ -3,6 +3,7 @@ import 'package:skar_admin/models/product_color.dart';
 import 'package:skar_admin/pages/product_colors/parts/selected_dimensions.dart';
 import 'package:skar_admin/pages/product_colors/parts/selected_product_color_images.dart';
 import 'package:skar_admin/styles/colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProductColorCard extends StatelessWidget {
   const ProductColorCard({super.key, required this.productColor});
@@ -11,6 +12,8 @@ class ProductColorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return Card(
       child: ClipRRect(
         borderRadius: BorderRadius.circular(8.0),
@@ -18,18 +21,38 @@ class ProductColorCard extends StatelessWidget {
           collapsedBackgroundColor: elevatedButtonColor,
           collapsedTextColor: Colors.white,
           title: Text(productColor.name),
+          expandedCrossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Saylanan olcegler: '),
+            TtitleWidget(text: lang.selectedDimensions),
             SelectedDimensions(
               selectedDimensions: productColor.selectedDimensions,
             ),
-            const Text('Saylanan suratlar: '),
+            const Divider(),
+            TtitleWidget(text: lang.selectedImages),
             SelectedProductColorImages(
               selectedImages: productColor.selectedImages,
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TtitleWidget extends StatelessWidget {
+  const TtitleWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child:
+          Text('$text :', style: const TextStyle(fontWeight: FontWeight.bold)),
     );
   }
 }
