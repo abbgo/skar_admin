636fd99229297c36dd087fb1f2175814297071d8
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 14:25:39 +0500

    fix: update ProductColorsBody

--- lib/pages/product_colors/parts/product_colors_body.dart
+++ lib/pages/product_colors/parts/product_colors_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/product_color.dart';
+import 'package:skar_admin/pages/parts/no_result.dart';
 import 'package:skar_admin/pages/product_colors/parts/product_color_card.dart';
 import 'package:skar_admin/providers/pages/add_or_update_product.dart';
 
@@ -11,15 +12,17 @@ class ProductColorsBody extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     List<ProductColor> productColors = ref.watch(productColorsProvider);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: ListView.builder(
-        itemCount: productColors.length,
-        itemBuilder: (context, index) {
-          ProductColor productColor = productColors[index];
-          return ProductColorCard(productColor: productColor);
-        },
-      ),
-    );
+    return productColors.isEmpty
+        ? const NoResult()
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView.builder(
+              itemCount: productColors.length,
+              itemBuilder: (context, index) {
+                ProductColor productColor = productColors[index];
+                return ProductColorCard(productColor: productColor);
+              },
+            ),
+          );
   }
 }
