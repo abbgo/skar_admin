6b62215c4cabc583718fddf5c9f97d16ba40baaa
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 16:08:28 +0500

    feat: add createProductProvider provider

--- lib/providers/api/product.dart
+++ lib/providers/api/product.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/helpers/methods/snackbars.dart';
 import 'package:skar_admin/models/product.dart';
+import 'package:skar_admin/providers/internet_connection.dart';
 import 'package:skar_admin/providers/local_storadge/setting.dart';
 import 'package:skar_admin/providers/pages/products.dart';
 import 'package:skar_admin/services/api/product.dart';
@@ -35,6 +38,40 @@ var fetchProductsProvider =
     } catch (e) {
       result = ResultProduct(error: e.toString());
     }
+    return result;
+  },
+);
+
+var createProductProvider =
+    FutureProvider.autoDispose.family<ResultProduct, ProductParams>(
+  (ref, arg) async {
+    ResultProduct result = ResultProduct.defaultResult();
+
+    try {
+      bool hasInternert =
+          await ref.read(checkInternetConnProvider(arg.context!).future);
+
+      if (hasInternert) {
+        String accessToken = await ref.read(accessTokenProvider);
+        ResultProduct resultShop = await ref
+            .read(productApiProvider)
+            .createProduct(accessToken: accessToken, product: arg.product!);
+
+        if (resultShop.error == 'auth error') {
+          await ref.read(accessTokenProvider.notifier).update('');
+          if (arg.context!.mounted) Navigator.pop(arg.context!);
+        }
+
+        if (resultShop.error == 'some error') {
+          if (arg.context!.mounted) showSomeErr(arg.context!);
+        }
+
+        result = resultShop;
+      }
+    } catch (e) {
+      result = ResultProduct(error: e.toString());
+    }
+
     return result;
   },
 );
