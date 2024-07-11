f614e355fbe2f4a52bf25e1593035cdc08644632
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 15:19:52 +0500

    fix: update AddOrUpdateProductButton part widget

--- lib/pages/add_or_update_product/parts/add_or_update_product_button.dart
+++ lib/pages/add_or_update_product/parts/add_or_update_product_button.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/helpers/methods/snackbars.dart';
+import 'package:skar_admin/models/category.dart';
+import 'package:skar_admin/models/product_color.dart';
+import 'package:skar_admin/providers/pages/add_or_update_product.dart';
+import 'package:skar_admin/providers/pages/category.dart';
 
-class AddOrUpdateProductButton extends StatelessWidget {
+class AddOrUpdateProductButton extends ConsumerWidget {
   const AddOrUpdateProductButton({
     super.key,
     required this.formKey,
@@ -21,14 +27,27 @@ class AddOrUpdateProductButton extends StatelessWidget {
   final String shopID;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
 
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.green,
       ),
-      onPressed: () {},
+      onPressed: () async {
+        List<Category> selectedCategories =
+            ref.read(selectedCategoriesProvider);
+
+        List<ProductColor> productColors = ref.read(productColorsProvider);
+
+        if (formKey.currentState?.validate() == true &&
+            selectedCategories.isNotEmpty &&
+            productColors.isNotEmpty) {
+          return;
+        }
+
+        if (context.mounted) showEnterInformationWarning(context);
+      },
       child: Text(lang.add),
     );
   }
