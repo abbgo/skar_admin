13b9f710243f8752f43157dbc3d70231a082be0d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 09:38:36 +0500

    feat: add ProductColorsNotifier , SelectedProductImagesNotifier , SelectedDimensionsNotifier notifiers

---
+++ lib/notifiers/pages/add_or_update_product.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/models/dimension.dart';
+import 'package:skar_admin/models/image.dart';
+import 'package:skar_admin/models/product_color.dart';
+
+class ProductColorsNotifier extends StateNotifier<List<ProductColor>> {
+  ProductColorsNotifier() : super([]);
+
+  Future<void> addProductColor(ProductColor productColor) async {
+    if (!state.contains(productColor)) {
+      state = [...state, productColor];
+    }
+  }
+}
+
+class SelectedProductImagesNotifier extends StateNotifier<List<SelectedImage>> {
+  SelectedProductImagesNotifier() : super([]);
+
+  Future<void> addImage(SelectedImage image) async {
+    if (!state.contains(image)) {
+      state = [...state, image];
+    }
+  }
+
+  Future<void> removeImage(SelectedImage image) async {
+    state = state.where((i) => i != image).toList();
+  }
+}
+
+class SelectedDimensionsNotifier extends StateNotifier<List<Dimension>> {
+  SelectedDimensionsNotifier() : super([]);
+
+  Future<void> addDimension(Dimension dimension) async {
+    if (!state.contains(dimension)) {
+      state = [...state, dimension];
+    }
+  }
+
+  Future<void> removeDimension(Dimension dimension) async {
+    state = state.where((i) => i != dimension).toList();
+  }
+}
