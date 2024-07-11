bd64b8da704b0a282c4bb410b8d492ee04db5e86
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-11 09:44:34 +0500

    feat: add PasswordVisibleNotifier notifier

--- lib/providers/pages/login.dart
+++ lib/providers/pages/login.dart
@@ -1,12 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class PasswordVisibleNotifier extends StateNotifier<bool> {
-  PasswordVisibleNotifier() : super(false);
-
-  void changeVisibility() {
-    state = !state;
-  }
-}
+import 'package:skar_admin/notifiers/pages/login.dart';
 
 var passwordVisibleProvider =
     StateNotifierProvider.autoDispose<PasswordVisibleNotifier, bool>(
