06a8d2d34b8981f7a19a5c380c9175db45521e47
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-13 15:13:23 +0500

    fix: update SelectedImages part widget

--- lib/pages/add_product_color/parts/selected_images.dart
+++ lib/pages/add_product_color/parts/selected_images.dart
@@ -8,72 +8,62 @@ import 'package:skar_admin/providers/parts/file_upload.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:skar_admin/services/api/image.dart';
 
-class SelectedImages extends StatefulWidget {
+class SelectedImages extends ConsumerWidget {
   const SelectedImages({super.key});
 
   @override
-  State<SelectedImages> createState() => _SelectedImagesState();
-}
-
-class _SelectedImagesState extends State<SelectedImages> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
 
-    return Consumer(
-      builder: (context, ref, child) {
-        List<SelectedImage> selectedImages = ref.watch(productImagesProvider);
-        bool loadSendImage = ref.watch(loadSendImageProvider);
+    List<SelectedImage> selectedImages = ref.watch(productImagesProvider);
+    bool loadSendImage = ref.watch(loadSendImageProvider);
 
-        return Expanded(
-          child: !loadSendImage
-              ? selectedImages.isEmpty
-                  ? Text(lang.noImage, textAlign: TextAlign.center)
-                  : ReorderableListView.builder(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        SelectedImage selectedImage = selectedImages[index];
-                        return Card(
-                          key: Key(selectedImage.path),
-                          child: ListTile(
-                            title: Image(
-                              image: FileImage(selectedImage.image!),
-                              height: 100,
-                            ),
-                            trailing: IconButton(
-                              onPressed: () async {
-                                ImageParams params = ImageParams(
-                                  oldImage: selectedImage.path,
-                                  context: context,
-                                );
-                                await ref
-                                    .watch(deleteImageProvider(params).future);
-                                await ref
-                                    .read(productImagesProvider.notifier)
-                                    .removeImage(selectedImage);
-                              },
-                              icon: const Icon(
-                                Icons.delete_forever,
-                                color: Colors.red,
-                                size: 30,
-                              ),
-                            ),
+    return Expanded(
+      child: !loadSendImage
+          ? selectedImages.isEmpty
+              ? Text(lang.noImage, textAlign: TextAlign.center)
+              : ReorderableListView.builder(
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    SelectedImage selectedImage = selectedImages[index];
+                    return Card(
+                      key: Key(selectedImage.path),
+                      child: ListTile(
+                        title: Image(
+                          image: FileImage(selectedImage.image!),
+                          height: 100,
+                        ),
+                        trailing: IconButton(
+                          onPressed: () async {
+                            ImageParams params = ImageParams(
+                              oldImage: selectedImage.path,
+                              context: context,
+                            );
+                            await ref.watch(deleteImageProvider(params).future);
+                            await ref
+                                .read(productImagesProvider.notifier)
+                                .removeImage(selectedImage);
+                          },
+                          icon: const Icon(
+                            Icons.delete_forever,
+                            color: Colors.red,
+                            size: 30,
                           ),
-                        );
-                      },
-                      itemCount: selectedImages.length,
-                      onReorder: (oldIndex, newIndex) {
-                        if (newIndex > oldIndex) {
-                          newIndex -= 1;
-                        }
-                        final SelectedImage item =
-                            selectedImages.removeAt(oldIndex);
-                        selectedImages.insert(newIndex, item);
-                      },
-                    )
-              : loadWidget,
-        );
-      },
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: selectedImages.length,
+                  onReorder: (oldIndex, newIndex) {
+                    if (newIndex > oldIndex) {
+                      newIndex -= 1;
+                    }
+                    final SelectedImage item =
+                        selectedImages.removeAt(oldIndex);
+                    selectedImages.insert(newIndex, item);
+                  },
+                )
+          : loadWidget,
     );
   }
 }
