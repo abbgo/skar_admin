3a02b2350231866c5dc2c94f8d88b76ac825b73f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-13 15:15:39 +0500

    fix: update ProductColorsBody part widget

--- lib/pages/product_colors/parts/product_colors_body.dart
+++ lib/pages/product_colors/parts/product_colors_body.dart
@@ -5,48 +5,36 @@ import 'package:skar_admin/pages/parts/no_result.dart';
 import 'package:skar_admin/pages/product_colors/parts/product_color_card.dart';
 import 'package:skar_admin/providers/pages/add_or_update_product.dart';
 
-class ProductColorsBody extends StatefulWidget {
+class ProductColorsBody extends ConsumerWidget {
   const ProductColorsBody({super.key});
 
   @override
-  State<ProductColorsBody> createState() => _ProductColorsBodyState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<ProductColor> productColors = ref.watch(productColorsProvider);
 
-class _ProductColorsBodyState extends State<ProductColorsBody> {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        List<ProductColor> productColors = ref.watch(productColorsProvider);
-
-        return productColors.isEmpty
-            ? const NoResult()
-            : Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: ReorderableListView.builder(
-                  itemBuilder: (context, index) {
-                    ProductColor productColor = productColors[index];
-                    return ProductColorCard(
-                      productColor: productColor,
-                      key: Key(productColor.name),
-                    );
-                  },
-                  itemCount: productColors.length,
-                  onReorderEnd: (index) =>
-                      productColors[index].copyWith(orderNumber: index + 1),
-                  onReorder: (oldIndex, newIndex) {
-                    setState(() {
-                      if (newIndex > oldIndex) {
-                        newIndex -= 1;
-                      }
-                      final ProductColor item =
-                          productColors.removeAt(oldIndex);
-                      productColors.insert(newIndex, item);
-                    });
-                  },
-                ),
-              );
-      },
-    );
+    return productColors.isEmpty
+        ? const NoResult()
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ReorderableListView.builder(
+              itemBuilder: (context, index) {
+                ProductColor productColor = productColors[index];
+                return ProductColorCard(
+                  productColor: productColor,
+                  key: Key(productColor.name),
+                );
+              },
+              itemCount: productColors.length,
+              onReorderEnd: (index) =>
+                  productColors[index].copyWith(orderNumber: index + 1),
+              onReorder: (oldIndex, newIndex) {
+                if (newIndex > oldIndex) {
+                  newIndex -= 1;
+                }
+                final ProductColor item = productColors.removeAt(oldIndex);
+                productColors.insert(newIndex, item);
+              },
+            ),
+          );
   }
 }
