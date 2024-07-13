39746e4e4d01d618a5eae6c630d1cb2b59f3e6cf
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-13 15:10:39 +0500

    fix: update SelectedImages part widget

--- lib/pages/add_product_color/parts/selected_images.dart
+++ lib/pages/add_product_color/parts/selected_images.dart
@@ -8,60 +8,72 @@ import 'package:skar_admin/providers/parts/file_upload.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:skar_admin/services/api/image.dart';
 
-class SelectedImages extends ConsumerWidget {
+class SelectedImages extends StatefulWidget {
   const SelectedImages({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<SelectedImages> createState() => _SelectedImagesState();
+}
+
+class _SelectedImagesState extends State<SelectedImages> {
+  @override
+  Widget build(BuildContext context) {
     var lang = AppLocalizations.of(context)!;
 
-    List<SelectedImage> selectedImages = ref.watch(productImagesProvider);
-    bool loadSendImage = ref.watch(loadSendImageProvider);
+    return Consumer(
+      builder: (context, ref, child) {
+        List<SelectedImage> selectedImages = ref.watch(productImagesProvider);
+        bool loadSendImage = ref.watch(loadSendImageProvider);
 
-    return Expanded(
-      child: !loadSendImage
-          ? selectedImages.isEmpty
-              ? Text(lang.noImage, textAlign: TextAlign.center)
-              : GridView.builder(
-                  itemCount: selectedImages.length,
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 2,
-                    mainAxisSpacing: 20,
-                    mainAxisExtent: 100,
-                  ),
-                  itemBuilder: (context, index) {
-                    SelectedImage selectedImage = selectedImages[index];
-                    return Row(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image(
-                          image: FileImage(selectedImage.image!),
-                          height: 100,
-                        ),
-                        IconButton(
-                          onPressed: () async {
-                            ImageParams params = ImageParams(
-                              oldImage: selectedImage.path,
-                              context: context,
-                            );
-                            await ref.watch(deleteImageProvider(params).future);
-                            await ref
-                                .read(productImagesProvider.notifier)
-                                .removeImage(selectedImage);
-                          },
-                          icon: const Icon(
-                            Icons.delete_forever,
-                            color: Colors.red,
+        return Expanded(
+          child: !loadSendImage
+              ? selectedImages.isEmpty
+                  ? Text(lang.noImage, textAlign: TextAlign.center)
+                  : ReorderableListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        SelectedImage selectedImage = selectedImages[index];
+                        return Card(
+                          key: Key(selectedImage.path),
+                          child: ListTile(
+                            title: Image(
+                              image: FileImage(selectedImage.image!),
+                              height: 100,
+                            ),
+                            trailing: IconButton(
+                              onPressed: () async {
+                                ImageParams params = ImageParams(
+                                  oldImage: selectedImage.path,
+                                  context: context,
+                                );
+                                await ref
+                                    .watch(deleteImageProvider(params).future);
+                                await ref
+                                    .read(productImagesProvider.notifier)
+                                    .removeImage(selectedImage);
+                              },
+                              icon: const Icon(
+                                Icons.delete_forever,
+                                color: Colors.red,
+                                size: 30,
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    );
-                  },
-                )
-          : loadWidget,
+                        );
+                      },
+                      itemCount: selectedImages.length,
+                      onReorder: (oldIndex, newIndex) {
+                        if (newIndex > oldIndex) {
+                          newIndex -= 1;
+                        }
+                        final SelectedImage item =
+                            selectedImages.removeAt(oldIndex);
+                        selectedImages.insert(newIndex, item);
+                      },
+                    )
+              : loadWidget,
+        );
+      },
     );
   }
 }
