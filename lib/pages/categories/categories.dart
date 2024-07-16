2a5cb0e5d7166d4187fedb27a717078f6cf1e358
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-16 15:37:46 +0500

    feat: add categorySearchProvider and hasCategoriesProvider providers

--- lib/pages/categories/categories.dart
+++ lib/pages/categories/categories.dart
@@ -3,18 +3,33 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/category.dart';
 import 'package:skar_admin/pages/categories/parts/result_categories.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar_admin/pages/parts/search_input.dart';
+import 'package:skar_admin/providers/pages/category.dart';
 
-class CategoriesPage extends ConsumerWidget {
+class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key, required this.childCategories});
 
   final List<Category> childCategories;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     var lang = AppLocalizations.of(context)!;
 
     return Scaffold(
-      appBar: AppBar(title: Text(lang.selectCategories), centerTitle: false),
+      appBar: AppBar(
+        title: Consumer(
+          builder: (context, ref, child) {
+            return SearchInput(
+              label: lang.searchBrand,
+              onSubmitted: (value) {
+                ref.read(categorySearchProvider.notifier).state = value;
+                ref.read(hasCategoriesProvider.notifier).state = true;
+              },
+            );
+          },
+        ),
+        centerTitle: false,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
         child: ResultCategories(childCategories: childCategories),
--- lib/providers/pages/category.dart
+++ lib/providers/pages/category.dart
@@ -2,6 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/models/category.dart';
 import 'package:skar_admin/notifiers/pages/category.dart';
 
+var categorySearchProvider = StateProvider.autoDispose<String>((ref) => '');
+var hasCategoriesProvider = StateProvider.autoDispose<bool>((ref) => true);
+
 var selectedCategoriesProvider =
     StateNotifierProvider<SelectedCategoriesNotifier, List<Category>>(
   (ref) => SelectedCategoriesNotifier(),
