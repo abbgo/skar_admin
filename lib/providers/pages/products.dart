de65a99441f3c7095110629df1aa13eafbdeb111
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-16 17:29:57 +0500

    feat: add productSearchProvider provider

--- lib/providers/pages/products.dart
+++ lib/providers/pages/products.dart
@@ -1,3 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var hasProductsProvider = StateProvider.autoDispose<bool>((ref) => true);
+var productSearchProvider = StateProvider.autoDispose<String>((ref) => '');
