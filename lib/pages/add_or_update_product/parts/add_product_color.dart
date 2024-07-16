1216e2822329576ef67247e4e64f26a171c3aa97
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-16 15:20:22 +0500

    fix: update AddProductColor

--- lib/pages/add_or_update_product/parts/add_product_color.dart
+++ lib/pages/add_or_update_product/parts/add_product_color.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/helpers/methods/navigation.dart';
+import 'package:skar_admin/models/product_color.dart';
 import 'package:skar_admin/pages/product_colors/product_colors.dart';
+import 'package:skar_admin/providers/pages/add_or_update_product.dart';
 
 class AddProductColor extends StatelessWidget {
   const AddProductColor({super.key});
@@ -18,6 +21,20 @@ class AddProductColor extends StatelessWidget {
           Text(
             '${lang.addProductColors} * : ',
             style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              List<ProductColor> productColors =
+                  ref.watch(productColorsProvider);
+
+              return Text(
+                '${lang.countProductTypes}: ${productColors.length}',
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            },
           ),
           SizedBox(
             width: double.infinity,
