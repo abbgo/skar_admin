54b93ce2efcc1eb1dae5b3dc02e45423b58ff906
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-17 14:46:09 +0500

    feat: add productsScrollControllerProvider and openProductdNavigateToTopButtonProvider providers

--- lib/providers/pages/products.dart
+++ lib/providers/pages/products.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var hasProductsProvider = StateProvider.autoDispose<bool>((ref) => true);
 var productSearchProvider = StateProvider<String>((ref) => '');
+
+var productsScrollControllerProvider =
+    StateProvider.autoDispose<ScrollController>((ref) {
+  final controller = ScrollController();
+  controller.addListener(() {
+    if (controller.position.pixels > 1000) {
+      ref.read(openProductdNavigateToTopButtonProvider.notifier).state = true;
+    } else {
+      ref.read(openProductdNavigateToTopButtonProvider.notifier).state = false;
+    }
+  });
+  return controller;
+});
+
+var openProductdNavigateToTopButtonProvider =
+    StateProvider.autoDispose<bool>((ref) => false);
