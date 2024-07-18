90c2cc220600b9ca7d4db6b1932551139714e123
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-18 14:30:57 +0500

    fix: update ProductPopUpMenu part widget

--- lib/pages/parts/product_list_tile/parts/product_pop_up_menu.dart
+++ lib/pages/parts/product_list_tile/parts/product_pop_up_menu.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar_admin/helpers/methods/navigation.dart';
 import 'package:skar_admin/helpers/methods/parts/shop_list_tile.dart';
+import 'package:skar_admin/helpers/methods/snackbars.dart';
+import 'package:skar_admin/models/product.dart';
 import 'package:skar_admin/pages/add_or_update_product/add_or_update_product.dart';
+import 'package:skar_admin/providers/api/product.dart';
+import 'package:skar_admin/providers/pages/products.dart';
+import 'package:skar_admin/services/api/product.dart';
 import 'package:skar_admin/styles/colors.dart';
 
-class ProductPopUpMenu extends StatelessWidget {
+class ProductPopUpMenu extends ConsumerWidget {
   const ProductPopUpMenu({
     super.key,
     required this.shopID,
@@ -16,7 +22,7 @@ class ProductPopUpMenu extends StatelessWidget {
   final String productID;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     var lang = AppLocalizations.of(context)!;
     List<String> texts = [lang.change, lang.moveToTrash];
 
@@ -27,7 +33,7 @@ class ProductPopUpMenu extends StatelessWidget {
         popUpMenuMethod(texts[0], Icons.edit),
         popUpMenuMethod(texts[1], Icons.auto_delete),
       ],
-      onSelected: (value) {
+      onSelected: (value) async {
         if (value == texts[0]) {
           goToPage(
             context,
@@ -38,8 +44,21 @@ class ProductPopUpMenu extends StatelessWidget {
         }
 
         if (value == texts[1]) {
-          // eger dukana degisli haryt yok bolsa
-          // dukany korzina gos
+          ref.read(loadDeleteProductProvider.notifier).state = true;
+
+          ProductParams params =
+              ProductParams(productID: productID, context: context);
+          ResultProduct resultProduct =
+              await ref.watch(deleteProductProvider(params).future);
+
+          ref.read(loadDeleteProductProvider.notifier).state = false;
+
+          if (resultProduct.error == '') {
+            ref.invalidate(fetchProductsProvider);
+            if (context.mounted) {
+              showSuccess(context, lang.informationDeletedSuccessfully);
+            }
+          }
           return;
         }
       },
