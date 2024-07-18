5eb74e60c11710e272d400bf06a419ea6efd540f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-18 17:18:52 +0500

    feat: add loadDeleteShopProvider provider

--- lib/providers/pages/shops.dart
+++ lib/providers/pages/shops.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var hasShopsProvider = StateProvider.autoDispose<bool>((ref) => true);
+
+final loadDeleteShopProvider = StateProvider.autoDispose<bool>((ref) => false);
