a7cc45a844da5fbd4d00f8ea40288f872914c641
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-23 12:06:56 +0500

    some commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skar_admin/database/config.dart';
-import 'package:skar_admin/helpers/map.dart';
 import 'package:skar_admin/pages/start.dart';
 import 'package:skar_admin/providers/local_storadge/setting.dart';
 import 'package:skar_admin/styles/theme/theme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,13 +20,6 @@ void main() async {
   await createDB(); // create database
   await dotenv.load(fileName: ".env"); // LOAD .ENV FILE
 
-  // asakdaky kot google map ucin
-  final GoogleMapsFlutterPlatform mapsImplementation =
-      GoogleMapsFlutterPlatform.instance;
-  if (mapsImplementation is GoogleMapsFlutterAndroid) {
-    mapsImplementation.useAndroidViewSurface = true;
-    await initializeMapRenderer();
-  }
   runApp(const ProviderScope(child: MyApp()));
 }
 
