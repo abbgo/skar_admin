14a84233ddd865c249940eb56e933f85882d1552
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-05 13:37:31 +0500

    feat: add ComplaintProductParams param class

--- lib/providers/api/complaint_product.dart
+++ lib/providers/api/complaint_product.dart
@@ -1,34 +1,37 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar_admin/helpers/functions/validation.dart';
+import 'package:skar_admin/models/complaint_product.dart';
+import 'package:skar_admin/providers/local_storadge/setting.dart';
 import 'package:skar_admin/services/api/complaint_product.dart';
 
 final complaintProductApiProvider =
     Provider<ComplaintProductApiService>((ref) => ComplaintProductApiService());
 
 var fetchBrendsProvider =
-    FutureProvider.autoDispose.family<ResultBrend, DefaultParams>(
+    FutureProvider.autoDispose.family<ResultComplaintProduct, DefaultParams>(
   (ref, arg) async {
-    ResultBrend result = ResultBrend.defaultResult();
+    ResultComplaintProduct result = ResultComplaintProduct.defaultResult();
 
     try {
-      String search = ref.watch(brendSearchProvider);
       String accessToken = await ref.read(accessTokenProvider);
-      ResultBrend resultBrend = await ref.read(brendApiProvider).fetchBrends(
-            accessToken: accessToken,
-            page: arg.page!,
-            isDeleted: arg.isDeleted!,
-            search: search,
-          );
+      ResultComplaintProduct resultBrend =
+          await ref.read(complaintProductApiProvider).fetchComplaintProducts(
+                accessToken: accessToken,
+                page: arg.page!,
+                isDeleted: arg.isDeleted!,
+                search: search,
+              );
 
       await wrongToken(resultBrend.error, ref, arg.context);
 
-      if (resultBrend.brends != null) {
-        ref.read(hasBrendProvider.notifier).state =
-            resultBrend.brends!.isNotEmpty;
-      }
+      // if (resultBrend.brends != null) {
+      //   ref.read(hasBrendProvider.notifier).state =
+      //       resultBrend.brends!.isNotEmpty;
+      // }
 
       result = resultBrend;
     } catch (e) {
-      result = ResultBrend(error: e.toString());
+      result = ResultComplaintProduct(error: e.toString());
     }
     return result;
   },
