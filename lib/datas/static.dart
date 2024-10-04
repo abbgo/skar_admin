cd7812fd42000ec97c1bf210bddc114819a14299
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-04 07:01:35 +0500

    feat: add Genders static class

--- lib/datas/static.dart
+++ lib/datas/static.dart
@@ -7,3 +7,11 @@ class CreatedStatuses {
   static const int rejected = 1;
   static const int success = 2;
 }
+
+class Genders {
+  Genders._();
+
+  static const int male = 0;
+  static const int female = 1;
+  static const int child = 2;
+}
